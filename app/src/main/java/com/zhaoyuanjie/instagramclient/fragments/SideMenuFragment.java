ec041b288902d85edb7c6328e01e6cd9a9812d12
赵 元杰 <qq157755587@gmail.com>
2015-03-24 17:12:55 +0800

    add login layout

--- app/src/main/java/com/zhaoyuanjie/instagramclient/fragments/SideMenuFragment.java
+++ app/src/main/java/com/zhaoyuanjie/instagramclient/fragments/SideMenuFragment.java
@@ -6,19 +6,41 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zhaoyuanjie.instagramclient.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  *
  * Created by zhaoyuanjie on 15/3/12.
  */
 public class SideMenuFragment extends Fragment {
+    @InjectView(R.id.account) View mAccount;
+    @InjectView(R.id.avatar) ImageView mAvatar;
+    @InjectView(R.id.login) TextView mLogin;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_side_menu, container, false);
-
+        ButterKnife.inject(this, view);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick(R.id.account)
+    public void login() {
+        Toast.makeText(getActivity(), "TODO login", Toast.LENGTH_SHORT).show();
     }
 }
